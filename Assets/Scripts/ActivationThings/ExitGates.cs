7518239c0af207ce8462741cb18a4c1d9226a1d6
Goblin1101 <trollface831@gmail.com>
2023-12-16 00:22:13 +0700

    Обработка коллизии снарядов и пропов

--- Assets/Scripts/ActivationThings/ExitGates.cs
+++ Assets/Scripts/ActivationThings/ExitGates.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExitGates : MonoBehaviour
+public class ExitGates : Gate
 {
     // Start is called before the first frame update
     void Start()
